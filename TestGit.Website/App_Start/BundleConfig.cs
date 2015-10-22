d750edcfb704816139befa9f7f25c04aa4d504f8
SiCannon <simon.cannon@gmail.com>
2015-10-22 15:48:51 +0100

    Added bootstrap less files and fixed LessTransform paths

---
+++ TestGit.Website/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Optimization;
+using TestGit.Website.Plumbing;
+
+namespace TestGit.Website.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            var appbundle = new Bundle("~/bundles/less").Include("~/Styles/site.less");
+            appbundle.Transforms.Add(new LessTransform("~/Styles"));
+            appbundle.Transforms.Add(new CssMinify());
+            bundles.Add(appbundle);
+
+            var bootstrapbundle = new Bundle("~/bundles/bootstrap").Include("~/Styles/bootstrap/bootstrap.less");
+            bootstrapbundle.Transforms.Add(new LessTransform("~/Styles/bootstrap"));
+            bootstrapbundle.Transforms.Add(new CssMinify());
+            bundles.Add(bootstrapbundle);
+        }
+    }
+}
---
+++ TestGit.Website/Plumbing/LessTransform.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Optimization;
+using dotless.Core;
+
+namespace TestGit.Website.Plumbing
+{
+    public class LessTransform : IBundleTransform
+    {
+        string path;
+
+        public LessTransform(string path)
+        {
+            this.path = HttpContext.Current.Server.MapPath(path);
+        }
+
+        public void Process(BundleContext context, BundleResponse response)
+        {
+            Directory.SetCurrentDirectory(path);
+            
+            response.Content = Less.Parse(response.Content);
+            response.ContentType = "text/css";
+        }
+    }
+}
